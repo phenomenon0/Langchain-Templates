d0f4361f87e4485e9b7078fe62c7c2a7ee04a5b7
Femi Adeniran <66181395+phenomenon0@users.noreply.github.com>
2024-01-24 15:55:23 -0600

    Update server.py
    MOVING CLOSER

--- app/server.py
+++ app/server.py
@@ -16,7 +16,7 @@ async def redirect_root_to_docs():
 
 # Edit this to add the chain you want to add
 add_routes(app, pirate_speak_chain, path="/pirate-speak")
-add_routes(app, csv_agent_chain, path="\csv-agent")
+add_routes(app, csv_agent_chain, path="/csv-agent")
 
 if __name__ == "__main__":
     import uvicorn
